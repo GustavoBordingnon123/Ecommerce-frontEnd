d752d875101175972a569a4cc7284d8dc3ca2a7e
unknown <gustavo.bordignon@outlook.com>
2023-12-20 23:42:43 -0300

    fix uppercase error/2

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -7,7 +7,7 @@ import CartItem from "./components/cartItem";
 import Summary from "./components/summary";
 import useCart from "@/hooks/useCart";
 
-const cartPage = () => {
+const CartPage = () => {
 
     const cart = useCart();
 
@@ -48,4 +48,4 @@ const cartPage = () => {
     )   
 }
 
-export default cartPage;
+export default CartPage;
