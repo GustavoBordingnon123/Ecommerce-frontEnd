22ffeeb6a8b875af72054f05d5894e646bcc8087
unknown <gustavo.bordignon@outlook.com>
2023-12-24 01:44:09 -0300

    fix gets

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -8,6 +8,8 @@ import MobileFilters from "./components/mobileFilters";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/noResults";
 import ProductCard from "@/components/ui/productCard";
+import getAuthors from "@/actions/getColors";
+import getPublishers from "@/actions/getSizes";
 
 export const revalidate = 0;
 
@@ -16,8 +18,8 @@ interface CategoryPageProps {
         categoryId: string;
     },
     searchParams: {
-        colorId?: string;
-        sizeId?: string;
+        authorId?: string;
+        publisherId?: string;
     }
 }
 
@@ -26,11 +28,11 @@ const CategoryPage: React.FC<CategoryPageProps> = async({params, searchParams}) 
 
     const products = await getProducts({
         categoryId: params.categoryId,
-        colorId: searchParams.colorId,
-        sizeId: searchParams.sizeId
+        authorId: searchParams.authorId,
+        publisherId: searchParams.publisherId
     })
-    const sizes = await getSizes();
-    const colors = await getColors();
+    const authors = await getAuthors();
+    const publishers = await getPublishers();
     const category = await getCategory(params.categoryId);
 
     return(
@@ -42,18 +44,18 @@ const CategoryPage: React.FC<CategoryPageProps> = async({params, searchParams}) 
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                         
-                        <MobileFilters sizes={sizes} colors={colors} />
+                        <MobileFilters authors={authors} publishers={publishers} />
 
                         <div className="hidden lg:block">
                             <Filter 
-                                valueKey="sizeId"
-                                name="Sizes"
-                                data={sizes}
+                                valueKey="publisherId"
+                                name="Publishers"
+                                data={publishers}
                             />
                               <Filter 
-                                valueKey="colorId"
-                                name="Colors"
-                                data={colors}
+                                valueKey="authorId"
+                                name="Authors"
+                                data={authors}
                             />
                         </div>
 
