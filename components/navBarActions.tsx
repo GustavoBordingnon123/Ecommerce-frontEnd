7d3e3274861d0953cfe8c231444d2db001ec086b
unknown <gustavo.bordignon@outlook.com>
2024-01-07 22:31:59 -0300

    fix localstorage2

--- components/navBarActions.tsx
+++ components/navBarActions.tsx
@@ -8,117 +8,115 @@ import { useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
 const NavBarActions = () => {
+  const router = useRouter();
+  const cart = useCart();
+  const removeAll = useCart((state) => state.removeAll);
 
-    const router = useRouter();
-    const cart = useCart();
-    const removeAll = useCart((state) => state.removeAll);
+  const [user, setUser] = useState("Faça seu login ou cadastre-se!");
+  const [isMounted, setIsMounted] = useState(false);
 
-    const [user, setUser] = useState("Faça seu login ou cadastre-se!");
-    const [isMounted, setIsMounted] = useState(false);
-
+  useEffect(() => {
+    setIsMounted(true);
 
     const userData = localStorage.getItem('user');
 
-    const goToProfile = () => {
-        if (userData) {
-            router.push('/profile');
-        } else {
-            router.push('/login');
-        }
-    };
-
-    const logOut = async () => {
-        await localStorage.removeItem('user');
-        setUser("Faça seu login ou cadastre-se!");
-        removeAll();
-        router.push('/login');
-        toast.success('Logout foi realizado com sucesso');
-    };
-
-    const handleLogout = async () => {
-        toast((t) => (
-            <span>
-                Tem certeza que deseja <b>sair?</b>
-                <br />
-
-                <div className="flex items-center justify-center gap-x-5 mt-1">
-
-                    <button
-                        onClick={() => { logOut(); toast.dismiss(t.id); }}
-                        className="text-green-600"
-                    >
-                        Sim
-                    </button>
-                    <button
-                        onClick={() => toast.dismiss(t.id)}
-                        className="text-red-600"
-                    >
-                        Não
-                    </button>
-
-                </div>
-
-            </span>
-        ));
-    };
-
-    useEffect(() => {
-        setIsMounted(true);
-        if (userData) {
-            const user = JSON.parse(userData);
-            setUser(`Olá, ${user.name}`);
-        }
-    }, [userData]);
-
-    if (!isMounted) {
-        return null;
+    if (userData) {
+      const user = JSON.parse(userData);
+      setUser(`Olá, ${user.name}`);
     }
+  }, [localStorage.getItem('user')]);
 
-    return (
-        <div className='ml-auto flex items-center gap-x-4'>
-            <div className="flex justify-center items-center gap-1">
-                <User
-                    size={32}
-                    color="white"
-                    className="border-2 border-white rounded-full cursor-pointer"
-                    onClick={goToProfile}
-                />
-                <p className="hidden md:block lg:block md:px-1 lg:px-1 text-sm text-white font-sm max-w-[150px]">
-                    <p onClick={goToProfile} className="cursor-pointer">{user}</p>
-                    {userData ? (
-                        <div className="flex items-center gap-x-1">
-                            <span
-                                className="text-sm text-white font-medium cursor-pointer hover:underline"
-                                onClick={goToProfile}
-                            >
-                                Minha conta
-                            </span>
-                            |
-                            <span
-                                className="text-sm text-white font-medium cursor-pointer hover:underline"
-                                onClick={handleLogout}
-                            >
-                                Sair
-                            </span>
-                        </div>
-                    ) : null}
-                </p>
-            </div>
+  if (!isMounted) {
+    return null;
+  }
 
-            <Button
-                className="flex items-center rounded-full bg-transparent px-4 py-2 border border-white border-2"
-                onClick={() => router.push('/cart')}
-            >
-                <ShoppingBag
-                    size={20}
-                    color="white"
-                />
-                <span className="ml-2 text-sm font-medium text-white">
-                    {cart.items.length}
-                </span>
-            </Button>
+  const userData = localStorage.getItem('user'); // Declare userData here
+
+  const goToProfile = () => {
+    if (userData) {
+      router.push('/profile');
+    } else {
+      router.push('/login');
+    }
+  };
+
+  const logOut = async () => {
+    await localStorage.removeItem('user');
+    setUser("Faça seu login ou cadastre-se!");
+    removeAll();
+    router.push('/login');
+    toast.success('Logout foi realizado com sucesso');
+  };
+
+  const handleLogout = async () => {
+    toast((t) => (
+      <span>
+        Tem certeza que deseja <b>sair?</b>
+        <br />
+
+        <div className="flex items-center justify-center gap-x-5 mt-1">
+          <button
+            onClick={() => { logOut(); toast.dismiss(t.id); }}
+            className="text-green-600"
+          >
+            Sim
+          </button>
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="text-red-600"
+          >
+            Não
+          </button>
         </div>
-    );
+      </span>
+    ));
+  };
+
+  return (
+    <div className='ml-auto flex items-center gap-x-4'>
+      <div className="flex justify-center items-center gap-1">
+        <User
+          size={32}
+          color="white"
+          className="border-2 border-white rounded-full cursor-pointer"
+          onClick={goToProfile}
+        />
+        <p className="hidden md:block lg:block md:px-1 lg:px-1 text-sm text-white font-sm max-w-[150px]">
+          <p onClick={goToProfile} className="cursor-pointer">{user}</p>
+          {userData ? (
+            <div className="flex items-center gap-x-1">
+              <span
+                className="text-sm text-white font-medium cursor-pointer hover:underline"
+                onClick={goToProfile}
+              >
+                Minha conta
+              </span>
+              |
+              <span
+                className="text-sm text-white font-medium cursor-pointer hover:underline"
+                onClick={handleLogout}
+              >
+                Sair
+              </span>
+            </div>
+          ) : null}
+        </p>
+      </div>
+
+      <Button
+        className="flex items-center rounded-full bg-transparent px-4 py-2 border border-white border-2"
+        onClick={() => router.push('/cart')}
+      >
+        <ShoppingBag
+          size={20}
+          color="white"
+        />
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
+      </Button>
+    </div>
+  );
 };
 
 export default NavBarActions;
