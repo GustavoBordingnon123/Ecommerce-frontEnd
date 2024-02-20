17f2b66d2113ebcf5af9b8b51da937e8ceee7832
MAYKON MAYKON <gustavo.bordignon@outlook.com>
2024-02-20 11:13:15 -0300

    add loading update

---
+++ app/(routes)/product/[productId]/loading.tsx
@@ -0,0 +1,6 @@
+import LoadingEskeleton from "@/components/ui/loading";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingEskeleton />
+}
---
+++ components/loading/categorySkeleton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "@/components/ui/container";
+
+const CategorySkeleton = () => {
+  return (
+    <div className="bg-white">
+        <Container>
+            <div className="">
+
+            </div>
+        </Container>
+    </div>
+  );
+};
+
+export default CategorySkeleton;
