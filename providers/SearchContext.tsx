290a5014eeae8585a02d54ee654adb08bb430434
unknown <gustavo.bordignon@outlook.com>
2024-04-10 16:07:22 -0300

    fix searchContext error deploy

--- providers/SearchContext.tsx
+++ providers/SearchContext.tsx
@@ -1,3 +1,34 @@
+// "use client";
+
+// // SearchContext.tsx
+// import React, { createContext, useContext, useState } from 'react';
+
+// interface SearchContextData {
+//     searchValue: string;
+//     setSearchValue: (value: string) => void;
+// }
+
+// const SearchContext = createContext<SearchContextData | undefined>(undefined);
+
+// export const SearchProvider: React.FC = ({ children }: any) => {
+    
+//     const [searchValue, setSearchValue] = useState('');
+
+//     return (
+//         <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+//             {children}
+//         </SearchContext.Provider>
+//     );
+// };
+
+// export const useSearch = () => {
+//     const context = useContext(SearchContext);
+//     if (!context) {
+//         throw new Error('useSearch must be used within a SearchProvider');
+//     }
+//     return context;
+// };
+
 "use client";
 
 // SearchContext.tsx
@@ -10,7 +41,7 @@ interface SearchContextData {
 
 const SearchContext = createContext<SearchContextData | undefined>(undefined);
 
-export const SearchProvider: React.FC = ({ children }: any) => {
+export const SearchProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     
     const [searchValue, setSearchValue] = useState('');
 
